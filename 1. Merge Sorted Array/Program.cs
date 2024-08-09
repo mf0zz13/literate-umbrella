b32d27a4997ebc688618017da770225305387055
mf0zz13 <michael.foster31@outlook.com>
2024-08-08 20:45:03 -0400

    Created Merge Algorithm

---
+++ 1. Merge Sorted Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCodeProblems
+{ 
+    class MergeSortedArray
+    {
+        static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int counter = 0;
+
+            for(int i = m; i < nums1.Length; i++)
+            {
+                nums1[i] = nums2[counter];
+                counter++;
+            }
+
+            Array.Sort(nums1);
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
+            int m = 3;
+
+            int[] nums2 = new int[] { 2, 5, 6 };
+            int n = 3;
+
+            Merge(nums1, m, nums2, n);
+        }
+    }
+}
