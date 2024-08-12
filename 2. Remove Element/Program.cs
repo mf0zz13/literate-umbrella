7af87a498938d9d275e4ae2e2670b678ccc07f2d
mf0zz13 <michael.foster31@outlook.com>
2024-08-12 15:03:47 -0400

    Added Solution for second problem

---
+++ 2. Remove Element/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LeetCodeProblems
+{
+    class RemoveElement
+    {
+        static int RemoveElements(int[] nums, int val)
+        {
+            int count = 0;
+            int position = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == val)
+                {
+                    int tempNum = nums[i];
+                    nums[i] = nums[nums.Length - 1 - position];
+                    nums[nums.Length - 1 - position] = tempNum;
+                    position++;
+                    count++;
+                }
+                if (nums[i] == val) { i--; count--; }
+                if (i + position == nums.Length - 1) break;
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 3, 2, 2, 3 };
+            int val = 3;
+            RemoveElements(nums, val);
+        }
+    }
+}
