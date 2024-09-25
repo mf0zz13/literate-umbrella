a90f5c71dc45cb8ed52f09dc6a90e07782fbfd57
mf0zz13 <michael.foster31@outlook.com>
2024-09-25 12:31:31 -0400

    Completed Find Highest Altitude

---
+++ 1732. Find the Highest Altitude/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FindTheHighestAltitude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] gain = { -5, 1, 5, 0, -7 };
+            Console.Write(LargestAltitude(gain));
+            Console.Read();
+        }
+
+        static int LargestAltitude(int[] gain)
+        {
+            int max = 0;
+            int curr = 0;
+
+            foreach (int num in gain)
+            {
+                curr += num;
+                max = Math.Max(max, curr);
+            }
+
+            return max;
+        }
+    }
+}
