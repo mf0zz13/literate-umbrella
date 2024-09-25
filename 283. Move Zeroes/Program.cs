3942f2c6f6ff0e797dc7455a9329a2bb8148844c
mf0zz13 <michael.foster31@outlook.com>
2024-09-25 11:52:25 -0400

    Completed Move Zeroes Problem

---
+++ 283. Move Zeroes/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace MoveZeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 1, 0, 2, 0, 3, 0, 4, 0, 5 };
+
+            MoveZeroes(nums);
+
+            foreach (int num in nums)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.Read();
+        }
+
+        static void MoveZeroes(int[] nums)
+        {
+            int writePos = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                switch (nums[i])
+                {
+                    case 0:
+                        break;
+                    default:
+                        nums[writePos] = nums[i];
+                        writePos += 1;
+                        break;
+                }
+            }
+
+            for (int i = writePos; i < nums.Length; i++)
+            {
+                nums[i] = 0;
+            }
+        }
+    }
+}
