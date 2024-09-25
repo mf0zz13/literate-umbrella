92689d834e3f40830262270e5727b47c62db5250
mf0zz13 <michael.foster31@outlook.com>
2024-09-25 12:03:22 -0400

    Completed Is Subsequence

---
+++ 392. Is Subsequence/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace IsSubsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "axc";
+            string t = "ahbgdc";
+
+            Console.Write(IsSubsequence(s, t));
+            Console.Read();
+        }
+
+        static bool IsSubsequence(string s, string t)
+        {
+            int sPos = 0;
+
+            if (s.Length == 0)
+                return true;
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (t[i] == s[sPos])
+                    sPos += 1;
+
+                if (sPos == s.Length)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
