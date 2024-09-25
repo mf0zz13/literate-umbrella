33a518874a7150222b9c9601d7c99327f1ec2083
mf0zz13 <michael.foster31@outlook.com>
2024-09-25 12:56:58 -0400

    Completed Find Pivot Index

---
+++ 724. Find Pivot Index/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace FindPivotIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { -1, -1, 0, 1, 1, 0 };
+
+            Console.Write(PivotIndex(nums));
+            Console.Read();
+        }
+
+        static int PivotIndex(int[] nums)
+        {
+            int rightSum = nums.Sum();
+            int leftSum = 0;
+
+            if (nums.Length == 1)
+                return 0;
+
+            rightSum -= nums[0];
+            if (rightSum == 0)
+                return 0;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                leftSum += nums[i - 1];
+                rightSum -= nums[i];
+                if (leftSum == rightSum)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
